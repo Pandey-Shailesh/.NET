f13d564ca423eb5642fb3a71cc72a594caad42b4
pandey-shailesh <pandeyshailesh2081998@gmail.com>
2023-07-11 10:34:20 +0530

    I create the Calculator Program in C#

---
+++ CalculatorApp/CalculatorApp/Calculator.cs
@@ -0,0 +1,41 @@
+﻿namespace CalculatorApp
+{
+    class Calculator
+    {
+        public static double doCalculation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // NaN means not a number
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+
+                case "s":
+                    result = num1 - num2;
+                    break;
+
+                case "m":
+
+                    result = num1 * num2;
+                    break;
+
+                case "d":
+                    //while (num2 == 0)
+                    //{
+                    //    Console.WriteLine("Please enter the non-zero number");
+                    //    num2 = Convert.ToDouble(Console.ReadLine());
+                    //}
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
