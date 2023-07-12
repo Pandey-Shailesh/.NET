1d61ad6c5dc93a76b4b88ce6b53cf8c0076da793
pandey-shailesh <pandeyshailesh2081998@gmail.com>
2023-07-12 10:07:41 +0530

    Learn about the Destructor

---
+++ DestructorExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using DestructorLibrary;
+
+namespace DestructorExample
+{
+    internal class Program
+    {
+        static void cleanUpAction()
+        {
+            using DestructorEx de = new DestructorEx();
+            de.databaseConnection();
+        }
+        static void Main(string[] args)
+        {
+
+            cleanUpAction();
+            Console.ReadKey();
+        } // destructor will called here
+
+    }
+}
---
+++ DestructorLibrary/DestructorEx.cs
@@ -0,0 +1,28 @@
+﻿namespace DestructorLibrary
+{
+    public class DestructorEx : System.IDisposable
+    {
+        // constructor
+
+        public DestructorEx()
+        {
+
+            Console.WriteLine("File is Connected or Open");
+        }
+
+        public void databaseConnection()
+        {
+            Console.WriteLine("Reading table data from the database");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Database Connection Closed");
+        }
+
+        ~DestructorEx()
+        {
+            Console.WriteLine("File Connection Closed");
+        }
+    }
+}
